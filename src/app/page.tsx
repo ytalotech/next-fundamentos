5d89bc53a861ff70bf863cd76389066a9cc82f35
Ytalo Lopes <ytalolopes@MacBook-Pro-de-Ytalo.local>
2024-06-19 22:51:17 -0300

    Suspense API no React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import { resolve } from "path";
+import { Suspense } from "react";
+
+import { GithubProfile } from "@/components/github-profile";
+import { LongWaitComponent } from "@/components/long-wait-component";
 
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-
-  const response = await fetch('https://api.github.com/users/ytalotech');
-  const user = await response.json();
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 
   return (
-    <pre>{JSON.stringify(user, null, 2)}</pre>
-  );
+    <div>
+      <h1>Home</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quam explicabo deserunt officia repellendus! Amet accusamus consequatur, modi consectetur ratione molestias sequi architecto voluptates? Pariatur sunt voluptatem distinctio ut voluptas.
+      </p>
+
+      {/* Suspense faz com que assim que já for carreegado seja exibido na tela o component, e não ter que esperar todos paara poder exibir */}
+      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
+        <LongWaitComponent />
+      </Suspense>
+
+      <Suspense fallback={<p>Carregando GithubProfile</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  )
 }
---
+++ src/components/github-profile.tsx
@@ -0,0 +1,10 @@
+export async function GithubProfile(){
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    const response = await fetch('https://api.github.com/users/ytalotech');
+    const user = await response.json();
+  
+    return (
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    );
+}
